a3d06061dc0c6d4969ce384e7470dcafecdf7fc3
hpl <hpl@moving>
2012-09-05 16:18:17 +0800

    new channel method

--- snscli.py
+++ snscli.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+try:
+    import json
+except ImportError:
+    import simplejson as json
+from os.path import abspath
+
+from snsapi.utils import JsonDict
 from snsapi.snspocket import SNSPocket
 sp = SNSPocket()
 
@@ -10,6 +17,7 @@ auth = lambda  *al, **ad : sp.auth(*al, **ad)
 home_timeline = lambda *al, **ad : sp.home_timeline(*al, **ad)
 update = lambda  *al, **ad : sp.update(*al, **ad)
 reply = lambda  *al, **ad : sp.reply(*al, **ad)
+new_channel = lambda : JsonDict(json.load(open(abspath('conf/init-channel.json.example'),'r')))
 
 #==== default initialization one may like ====
 
