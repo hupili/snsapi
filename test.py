c8d3006225e32935eeeaa21e865582f9eae18911
hpl <hpl@moving>
2012-09-05 12:22:02 +0800

    test shortcuts modification

--- test.py
+++ test.py
@@ -1,16 +1,23 @@
-
-
 from snsapi.snspocket import SNSPocket
+from snsapi.utils import console_input,console_output
 
-s = SNSPocket()
-s.read_config()
+sp = SNSPocket()
+sp.read_config()
 #s.list(verbose = True)
-s.list()
-s.auth()
-s_list = s.home_timeline()
+sp.list()
+sp.auth()
+
+#======= print statuses =====
+s_list = sp.home_timeline()
 #for s in s_list:
 #    print unicode(s)
 #print unicode(s_list)
 #print str(s_list)
 print s_list
 
+#======= update status =====
+#print "input something to update:==="
+#s.update(console_input())
+
+#======= reply status ====
+#sp.reply(s_list[0].ID, 'haha')
