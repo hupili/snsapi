385ea6ba03f4b12ddea1bfa826cb0ff6914e03a3
hupili <hpl1989@gmail.com>
2013-01-21 00:27:00 +0800

    add more sample tests

--- tests/test_snsbase.py
+++ tests/test_snsbase.py
@@ -23,6 +23,18 @@ class TestSNSBase(TestBase):
     def teardown(self):
         pass
 
-    def test_snsbase_1(self):
-        eq_(2, len(SNSBase.new_channel()), WRONG_RESULT_ERROR)
+    def test_snsbase_new_channel_normal(self):
+        nc = SNSBase.new_channel()
+        eq_(2, len(nc), WRONG_RESULT_ERROR)
+        ok_(('channel_name' in nc), WRONG_RESULT_ERROR)
+        ok_(('open' in nc), WRONG_RESULT_ERROR)
+
+    def test_snsbase_new_channel_full(self):
+        nc = SNSBase.new_channel(full=True)
+        print nc
+        eq_(4, len(nc), WRONG_RESULT_ERROR)
+        ok_(('channel_name' in nc), WRONG_RESULT_ERROR)
+        ok_(('open' in nc), WRONG_RESULT_ERROR)
+        ok_(('description' in nc), WRONG_RESULT_ERROR)
+        ok_(('methods' in nc), WRONG_RESULT_ERROR)
 
