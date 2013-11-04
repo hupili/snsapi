556610a76ed678ea4c3bd23d0760fc2da66d7ff3
hugochan <hugochan2013@gmail.com>
2013-11-04 23:18:53 +0800

    a patch for compatible with windows

--- snsapi/plugin_trial/__init__.py
+++ snsapi/plugin_trial/__init__.py
@@ -3,13 +3,16 @@ import inspect
 import os
 import glob
 import snsapi.snsbase
-
+import platform
 
 fpath = os.path.abspath(__file__)
 for i in glob.glob(os.path.dirname(fpath) + "/*.py"):
     if i[-11:] == "__init__.py":
         continue
-    ss = __import__(__name__ + '.' + i[:-3].split('/')[-1], fromlist=["*"])
+    if platform.system() == "Windows":
+    	ss = __import__(__name__ + '.' + i[:-3].split('\\')[-1], fromlist=["*"])
+    else:
+    	ss = __import__(__name__ + '.' + i[:-3].split('/')[-1], fromlist=["*"])
     for i in dir(ss):
         if inspect.isclass(getattr(ss, i)) and issubclass(getattr(ss, i), snsapi.snsbase.SNSBase) and getattr(ss, i) != snsapi.snsbase.SNSBase:
             globals()[i] = getattr(ss, i)
